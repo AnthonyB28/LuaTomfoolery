bb344cf039bb2bf80d1346bdd2fd388b603ee3e7
Anthony Barranco <anthony@lssclan.org>
2014-10-02 19:01:09 -0400

    Semicolons
    Too cool for school

--- factorial.lua
+++ factorial.lua
@@ -1,12 +1,12 @@
 function Factorial(n)
 	if n == 0 then
-		return 1
+		return 1;
 	else
-		return n * Factorial(n-1)
+		return n * Factorial(n-1);
 	end
 end
 
-print("enter a number")
-inputNumber = io.read("*number")
-factorialNumber = Factorial(inputNumber)
-print(inputNumber .. "'s factorial is " .. factorialNumber)
+print("enter a number");
+inputNumber = io.read("*number");
+factorialNumber = Factorial(inputNumber);
+print(inputNumber .. "'s factorial is " .. factorialNumber);
