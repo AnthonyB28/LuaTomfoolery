e18f0e3d6b7bc9dbd36dcaca7441d2db72b4ef10
Anthony Barranco <anthony@lssclan.org>
2014-10-02 18:47:48 -0400

    Factorial
    Simple n! function

---
+++ factorial.lua
@@ -0,0 +1,12 @@
+function Factorial(n)
+	if n == 0 then
+		return 1
+	else
+		return n * Factorial(n-1)
+	end
+end
+
+print("enter a number")
+inputNumber = io.read("*number")
+factorialNumber = Factorial(inputNumber)
+print(inputNumber .. "'s factorial is " .. factorialNumber)
